819abe435e835f9d7cc6be770cfccb93f9115edd
FilbertSM <filbertsm7@gmail.com>
2025-12-18 17:01:00 +0700

    Database Config

--- server/configs/db.py
+++ server/configs/db.py
@@ -1,3 +1,13 @@
+import os
+from dotenv import load_dotenv
 from pymongo import AsyncMongoClient
 from beanie import init_beanie
+from models.model import User, Message
 
+# Import Mongo URI
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+async def init_db():
+    client = AsyncMongoClient(MONGO_URI)
+    await init_beanie(database=client.blog, document_models=[User, Message])
